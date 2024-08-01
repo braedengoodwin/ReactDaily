0392d87cf11848ba98227d9d9bc6ac83650a2965
braedengoodwin <braedenagoodwin@hotmail.com>
2024-08-01 18:14:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-800">
+      <header className="bg-white ">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-800">My Vite + React App</h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4">
+              <p className="text-center text-gray-500 text-xl">
+                Your content goes here
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
